5b20393c165ac3fb313bff143f08e258acc0e817
Vladislav Lyubimov <104612753+SmartPotato21@users.noreply.github.com>
2025-06-02 07:01:18 -0500

    logout button added

--- webapp/db/models.py
+++ webapp/db/models.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from webapp.db import db
+from flask_login import UserMixin, current_user
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String, unique=False)
--- webapp/main.py
+++ webapp/main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, redirect, url_for, render_template, flash, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
+
 from webapp import app
+
 import bcrypt
 
 from webapp.db import db
@@ -8,6 +11,9 @@ from webapp.db.models import User
 from webapp.db.manage import addUser
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login_page'
 
 
 @app.route("/")
@@ -45,15 +51,26 @@ def login_page():
             username = request.form.get("username")
             password = request.form.get("password")
             
+            
+            
             found_user = User.query.filter_by(username=username).first()
             if not found_user:
                 return "User not found"
+            
+            session['username'] = username
+            session['password'] = password
+            
+            print(found_user.id)
             
             userBytes = password.encode('utf-8')
             result = bcrypt.checkpw(userBytes, found_user.password.encode('utf-8'))
             
             if result:
-                return render_template('chat.html', username=username)
+                
+                login_user(found_user)
+                print("User logged in")
+                
+                return redirect('/chat')
             else:
                 return "Incorrect password"
             
@@ -62,8 +79,37 @@ def login_page():
     return render_template('login.html',show = active_tab)
 
 
-@app.route("/chat")
+@app.route("/chat",methods = ['GET','POST'])
+@login_required
 def chat_page():
+    print(current_user.is_authenticated)
+    if not current_user.is_authenticated:
+        return redirect('/login')
+
+    if request.method == 'POST':
+        if 'logout_select' in request.form:
+            return redirect('/logout')
+
+    return render_template('chat.html', username = current_user.username)
 
 
-    return "in chat"
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+def logout_user():
+    
+    if '_fresh' in session:
+        session.pop('_fresh')
+    
+        
+        
+        
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    session.clear()
+    flash('You have been logged out.', 'success')
+    return redirect("/login")
