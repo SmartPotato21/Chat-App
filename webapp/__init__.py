1b9a55a9759fc636b395de7823936f144ffcaf30
Vladislav Lyubimov <104612753+SmartPotato21@users.noreply.github.com>
2025-06-04 09:29:34 -0500

    securtiy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'verysecret'
 
 # Optionally, import your routes to bind them to the app
 from webapp import main
